5647eb0289b53b38ed10e2e3f8a553e3b38ccbc6
Ivan Kelber <ivan_kelber@brown.edu>
2016-03-07 19:03:42 -0500

    offense normalization

--- data/NewYork/cleaning/cleanNY.py
+++ data/NewYork/cleaning/cleanNY.py
@@ -35,28 +35,15 @@ def main():
 		month_index = header.index("Occurrence Month")
 		day_index = header.index("Occurrence Day")
 		offense_index = header.index("Offense")
-		classification_index = header.index("Offense Classification")
 		borough_index = header.index("Borough")
 		location_index = header.index("Location 1")
 
 		#Headers
-		fact_writer.writerow(["ID","Time_ID","Date_ID","Offense_ID","Location_ID"])
+		fact_writer.writerow(["ID","City_ID","Time_ID","Date_ID","Offense_ID","Location_ID"])
 		time_writer.writerow(["Time_ID","Hour","Minute"])
 		date_writer.writerow(["Date_ID","Day of Week","Month","Day","Year"])
-		offense_writer.writerow(["Offense_ID","Offense","Classification"])
+		offense_writer.writerow(["Offense_ID","Offense"])
 		location_writer.writerow(["Location_ID","Latitude","Longitude"])
-
-		#list of unique fields (for use in snowflake schema)
-		# date_list = {}
-		# dow_list = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-		# month_list = set(["January","February","March","April","May","June",\
-		# "July","August","September","October","November","December"])
-
-		# borough_list = ["Bronx","Brooklyn","Manhattan","Queens","Staten Island"]
-		# x_list = []
-		# y_list = []
-		# lat_list = []
-		# long_list = []
 
 		time_list = {}
 		date_list = {}
@@ -91,6 +78,7 @@ def main():
 				Year = row[year_index]
 				date_key = ','.join([Day_of_Week,Month,Day,Year])
 				date_id = -1
+
 				try:
 					date_id = date_list[date_key]
 				except KeyError:
@@ -100,15 +88,22 @@ def main():
 
 				#offense (will be used for calculating severity)
 				Offense = row[offense_index]
-				Classification = row[classification_index]
-				offense_key = ','.join([Offense,Classification])
+				if(Offense == 'RAPE'):
+					Offense = 'CRIM SEXUAL ASSAULT'
+				elif(Offense == 'FELONY ASSAULT'):
+					Offense = 'ASSAULT'
+				elif('LARCENY' in Offense):
+					Offense = 'LARCENY'
+				elif(Offense == 'MURDER'):
+					Offense = 'HOMICIDE'
+				offense_key = ','.join([Offense])
 				offense_id = -1
 				try:
 					offense_id = offense_list[offense_key]
 				except KeyError:
 					offense_list[offense_key] = len(offense_list)
 					offense_id = offense_list[offense_key]
-					offense_writer.writerow([offense_id,Offense,Classification])
+					offense_writer.writerow([offense_id,Offense])
 
 
 				#location
@@ -124,7 +119,7 @@ def main():
 					location_id = location_list[location_key]
 					location_writer.writerow([location_id,Latitude,Longitude])
 	 
-				fact_writer.writerow([ID,time_id,date_id,offense_id,location_id])
+				fact_writer.writerow([ID,1,time_id,date_id,offense_id,location_id])
 			
 
 	
