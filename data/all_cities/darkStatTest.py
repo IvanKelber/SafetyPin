f0ae66a1719a3d1258efdf5c57473d9181007713
jch13 <julie_helmers@brown.edu>
2016-03-21 22:11:56 -0400

    Daylight/darkness statistical testing (work in progress)

---
+++ data/all_cities/darkStatTest.py
@@ -0,0 +1,29 @@
+import csv
+import isItDark
+
+def main():
+
+     cities = ['Boston', 'Chicago', 'Denver', 'NewYork', 'Philly']
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
