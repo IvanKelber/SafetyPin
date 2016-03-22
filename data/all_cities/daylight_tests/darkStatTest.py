83aba21773c5875895f7fb2e50cece9ca9430004
jch13 <julie_helmers@brown.edu>
2016-03-22 19:57:00 -0400

    Updated daylight tests to break down by type of crime, also corrected for daylight savings time

--- data/all_cities/daylight_tests/darkStatTest.py
+++ data/all_cities/daylight_tests/darkStatTest.py
@@ -3,8 +3,6 @@ from isItDark import isItDark
 import sqlite3
 from scipy import stats
 
-# See "TO DO" from isItDark.py
-
 def main():
 
     # Things that I'm hard-coding because I'm feeling lazy...
@@ -15,50 +13,91 @@ def main():
     conn = sqlite3.connect("../crime.db")
     cursor = conn.cursor();
 
+    # Query our database to build a dictionary of offense IDs to offense names
+    crimes = {}
+    cursor.execute("select * from offense")
+    for crime in cursor.fetchall():
+            crimes[crime[0]] = crime[1].encode('utf-8')
+
+    # Now, what we actually care about:
     # Query our database to get hour, minute, month, day, and year for all crimes in all cities
     events = []
-    cursor.execute("select city_id,hour,minute,month,day,year from (select * from fact \
+    cursor.execute("select city_id,offense_id,hour,minute,month,day,year from (select * from fact \
             inner join time \
             on fact.time_id = time.id) time_join \
         inner join date \
         on time_join.date_id = date.id")
 
     # Initialize our counts for daylight and darkness
+    # Total
     light_total = 0
     dark_total = 0
+
+    # By city
     light_by_city = {}
     dark_by_city = {}
     for city_id,city in cities.items():
         light_by_city[city] = 0
         dark_by_city[city] = 0
 
-    # Count the number of crimes in the daylight and the darkness
-    allcrimes = cursor.fetchall()
+    # By type of crime
+    light_by_crime = {}
+    dark_by_crime = {}
+    for crime_id,crime in crimes.items():
+        light_by_crime[crime] = 0
+        dark_by_crime[crime] = 0
 
+    # Count the number of crimes in the daylight and the darkness
+    # Total, by city, and by type of crime
+    allcrimes = cursor.fetchall()
     for event in allcrimes:
         city = cities[event[0]]
-        hour = event[1] / 100
-        minute = event[2]
-        month = months[event[3].encode('utf-8')]
-        day = event[4]
-        year = event[5]
-        if isItDark(city, month, day, year, hour, minute):
+        crime = crimes[event[1]].encode('utf-8')
+        hour = event[2] / 100
+        minute = event[3]
+        month = months[event[4].encode('utf-8')]
+        day = event[5]
+        year = event[6]
+        if isItDark(city, month, day, year, hour, minute): # Darkness
             dark_total += 1
             dark_by_city[city] += 1
-        elif ~isItDark(city, month, day, year, hour, minute):
+            dark_by_crime[crime] += 1
+        elif ~isItDark(city, month, day, year, hour, minute): # Daylight
             light_total += 1
             light_by_city[city] += 1
+            light_by_crime[crime] += 1
 
     # Perform binomial tests
-    # Null hypothesis: The probability of a crime being committed in the daylight/darkness is 0.5    for 
+    # Null hypothesis: The probability of a crime being committed in the daylight/darkness is 0.5
+    # Total
+    print '------------------------------------------------'
+    print '----- Across all cities and types of crime -----'
+    print '------------------------------------------------'
     pval_total = stats.binom_test([light_total,dark_total])
-    print 'p-value for all cities ' + str(pval_total)
-
-    pval = {}
+    print 'Daylight: ' + str(light_total) + ' | Darkness: ' + str(dark_total)
+    print 'P-Value: ' + str(pval_total)
+    
+    # By city
+    print '------------------------------------------------'
+    print '----- By city, across all types of crime -----'
+    print '------------------------------------------------'
+    pval_by_city = {}
     for city_id,city in cities.items():
-        pval[city] = stats.binom_test([light_by_city[city],dark_by_city[city]])
-        print 'p-value for ' + city + ' ' + str(pval[city])
+        pval_by_city[city] = stats.binom_test([light_by_city[city],dark_by_city[city]])
+        print city.upper()
+        print 'Daylight: ' + str(light_by_city[city]) + ' | Darkness: ' + str(dark_by_city[city])
+        print 'P-Value: ' + str(pval_by_city[city])
 
-
+    # By type of crime
+    print '------------------------------------------------'
+    print '----- By type of crime, across all cities -----'
+    print '------------------------------------------------'
+    pval_by_crime = {}
+    for crime_id,crime in crimes.items():
+        pval_by_crime[crime] = stats.binom_test([light_by_crime[crime],dark_by_crime[crime]])
+        print crime
+        print 'Daylight: ' + str(light_by_crime[crime]) + ' | Darkness: ' + str(dark_by_crime[crime])
+        print 'P-Value: ' + str(pval_by_crime[crime])
+    
 if __name__ == '__main__':
     main()
