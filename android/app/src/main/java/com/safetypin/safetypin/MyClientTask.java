fe2986df829a811b76ceb84cc80a4e61432c9f37
Ronallen <ivankelber@gmail.com>
2016-05-02 23:01:01 -0400

    Connects to server when second point is entered.

--- android/app/src/main/java/com/safetypin/safetypin/MyClientTask.java
+++ android/app/src/main/java/com/safetypin/safetypin/MyClientTask.java
@@ -1,7 +1,12 @@
 package com.safetypin.safetypin;
 
 import android.os.AsyncTask;
-import android.widget.TextView;
+import android.util.Log;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.BufferedReader;
 import java.io.DataInputStream;
@@ -17,15 +22,17 @@ public class MyClientTask extends AsyncTask<Void, Void, Void> {
     String dstAddress;
     int dstPort;
     String response;
-    TextView textView;
+//    TextView textView;
+    GoogleMap mMap;
 //    FileListView fileListView;
     String message;
 
-    MyClientTask(String addr, int port, TextView textView, String message) {
+    MyClientTask(String addr, int port, GoogleMap mMap, String message) {
         dstAddress = addr;
         dstPort = port;
-        this.textView = textView;
+//        this.textView = textView;
         this.message = message;
+        this.mMap = mMap;
     }
 
     @Override
@@ -61,7 +68,13 @@ public class MyClientTask extends AsyncTask<Void, Void, Void> {
     protected void onPostExecute(Void result) {
         if(response != null) {
 //            String[] fileArray = response.substring(1, response.length() - 2).split(",");
-            textView.setText(response);
+//            textView.setText(response);
+            String[] latlng = response.split(",");
+            Log .e("RESPONSE",response);
+            LatLng l = new LatLng(Double.parseDouble(latlng[0]),Double.parseDouble(latlng[1]));
+            Marker m = mMap.addMarker(new MarkerOptions().position(l).title("Destination").draggable(true));
+
+
 //            fileListView.setFiles(Arrays.asList(fileArray));
         }
         super.onPostExecute(result);
