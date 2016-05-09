c4b4909350607813a0aee80fd99a172dbb428c8d
Dr. Dronika Solanki <solanki.dronika2007@gmail.com>
2016-05-09 14:15:02 -0400

    changes

--- data/NewYork/cleaning/cleanNY.py
+++ data/NewYork/cleaning/cleanNY.py
@@ -12,18 +12,30 @@ def main():
 		api_key = api.readline()
 
 	
+<<<<<<< Updated upstream
 	with open('../NewYorkMajorCrime.csv', 'rb') as ny, \
 	 open('fact_table2.csv','wb') as fact, \
 	 open('time_table2.csv', 'wb') as time, \
 	 open('date_table2.csv', 'wb') as date, \
 	 open('location_table2.csv','wb') as location, \
 	 open('offense_table2.csv','wb') as offense:
+=======
+	with open('NewYorkMajorCrime.csv', 'rb') as ny, \
+	 open('fact_table.csv','wb') as fact, \
+	 open('time_table.csv', 'wb') as time, \
+	 open('date_table.csv', 'wb') as date, \
+	 open('location_table.csv','wb') as location, \
+	 open('offense_table.csv','wb') as offense, \
+	 open('CrimeNY.csv','wb') as crimeNY:
+
+>>>>>>> Stashed changes
 		reader = csv.reader(ny)
 		fact_writer = csv.writer(fact)
 		time_writer = csv.writer(time)
 		date_writer = csv.writer(date)
 		location_writer = csv.writer(location)
 		offense_writer = csv.writer(offense)
+		crime_writer = csv.writer(crimeNY)
 
 
 		header = next(reader,None)
@@ -44,6 +56,7 @@ def main():
 		date_writer.writerow(["Date_ID","Day of Week","Month","Day","Year"])
 		offense_writer.writerow(["Offense_ID","Offense"])
 		location_writer.writerow(["Location_ID","Latitude","Longitude"])
+		crime_writer.writerow(["ID","Offense","Day of Week","Date","Time"])
 
 		time_list = {}
 		date_list = {}
@@ -76,6 +89,7 @@ def main():
 			# 		time_writer.writerow([time_id,Hour,Minute])
 
 				
+<<<<<<< Updated upstream
 			# 	#date
 			# 	Day_of_Week = row[dow_index]
 			# 	Month = row[month_index]
@@ -126,6 +140,63 @@ def main():
 			# 		location_writer.writerow([location_id,Latitude,Longitude])
 	 
 			# 	fact_writer.writerow([ID,1,time_id,date_id,offense_id,location_id])
+=======
+				#date
+				Day_of_Week = row[dow_index]
+				Month = row[month_index]
+				Day = row[day_index]
+				Year = row[year_index]
+				date_key = ','.join([Day_of_Week,Month,Day,Year])
+				date_id = -1
+
+				try:
+					date_id = date_list[date_key]
+				except KeyError:
+					date_list[date_key] = len(date_list)
+					date_id = date_list[date_key]
+					date_writer.writerow([date_id,Day_of_Week,Month,Day,Year])
+
+				#offense (will be used for calculating severity)
+				Offense = row[offense_index]
+				if(Offense == 'RAPE'):
+					Offense = 'CRIM SEXUAL ASSAULT'
+				elif(Offense == 'FELONY ASSAULT'):
+					Offense = 'ASSAULT'
+				elif('LARCENY' in Offense):
+					Offense = 'THEFT'
+				elif(Offense == 'MURDER'):
+					Offense = 'HOMICIDE'
+				offense_key = ','.join([Offense])
+				offense_id = -1
+				try:
+					offense_id = offense_list[offense_key]
+				except KeyError:
+					offense_list[offense_key] = len(offense_list)
+					offense_id = offense_list[offense_key]
+					offense_writer.writerow([offense_id,Offense,])
+
+
+				#location
+				Location = eval(row[location_index])
+				Latitude = Location[0]
+				Longitude = Location[1]
+				location_key = ','.join([str(Location)])
+				location_id = -1
+				try:
+					location_id = location_list[location_key]
+				except KeyError:
+					location_list[location_key] = len(location_list)
+					location_id = location_list[location_key]
+					location_writer.writerow([location_id,Latitude,Longitude])
+
+				datime = row[date_index].split()
+				time = datime[1]+" "+ datime[2]
+				in_time = datetime.strptime(time, "%I:%M:%S %p")
+				out_time = datetime.strftime(in_time, "%H:%M")
+	 
+				fact_writer.writerow([ID,1,time_id,date_id,offense_id,location_id])
+				crime_write.writerow([ID,offence,Day_of_Week,datime[0],out_time])
+>>>>>>> Stashed changes
 			
 
 	
---
+++ data/NewYork/dronika_clean/cleanNY.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+import sys
+import csv
+import re
+import operator
+#from apiclient.discovery import build
+from datetime import *
+
+api_key = ''
+
+def main():
+	# with open('../../../api_keys/google_api_key.txt', 'rb') as api:
+	# 	api_key = api.readline()
+
+	
+	with open('NewYorkMajorCrime.csv', 'rb') as ny, \
+	 open('fact_table.csv','wb') as fact, \
+	 open('time_table.csv', 'wb') as time, \
+	 open('date_table.csv', 'wb') as date, \
+	 open('location_table.csv','wb') as location, \
+	 open('offense_table.csv','wb') as offense, \
+	 open('CrimeNY.csv','wb') as crimeNY:
+
+		reader = csv.reader(ny)
+		fact_writer = csv.writer(fact)
+		time_writer = csv.writer(time)
+		date_writer = csv.writer(date)
+		location_writer = csv.writer(location)
+		offense_writer = csv.writer(offense)
+		crime_writer = csv.writer(crimeNY)
+
+
+		header = next(reader,None)
+		id_index = header.index("OBJECTID")
+		date_index = header.index("Occurrence Date")
+		dow_index = header.index("Day of Week")
+		hour_index = header.index("Occurrence Hour")
+		year_index = header.index("Occurrence Year")
+		month_index = header.index("Occurrence Month")
+		day_index = header.index("Occurrence Day")
+		offense_index = header.index("Offense")
+		borough_index = header.index("Borough")
+		location_index = header.index("Location 1")
+
+		#Headers
+		fact_writer.writerow(["ID","City_ID","Time_ID","Date_ID","Offense_ID","Location_ID"])
+		time_writer.writerow(["Time_ID","Hour","Minute"])
+		date_writer.writerow(["Date_ID","Day of Week","Month","Day","Year"])
+		offense_writer.writerow(["Offense_ID","Offense"])
+		location_writer.writerow(["Location_ID","Latitude","Longitude"])
+		crime_writer.writerow(["ID","Offense","Day of Week","Date","Time"])
+
+		time_list = {}
+		date_list = {}
+		offense_list = {}
+		location_list = {}
+
+		count = 0
+		for row in reader:
+			#need to remove occurrences before 2015 because of small sample size
+			if(eval(row[year_index]) >= 2015):
+				count += 1
+				ID = count
+				#time
+				Date = row[date_index]
+				Hour = row[hour_index]
+				if(Hour[0] == '0'):
+					Hour = Hour[1:]
+				minute_pattern = re.compile(r'.*:(\d\d):\d\d.*')
+				Minute = minute_pattern.search(Date).group(1)
+				time_key = ','.join([Hour,Minute])
+				time_id = -1
+				try:
+					time_id = time_list[time_key]
+				except KeyError:
+					time_list[time_key] = len(time_list)
+					time_id = time_list[time_key]
+					time_writer.writerow([time_id,Hour,Minute])
+
+				
+				#date
+				Day_of_Week = row[dow_index]
+				Month = row[month_index]
+				Day = row[day_index]
+				Year = row[year_index]
+				date_key = ','.join([Day_of_Week,Month,Day,Year])
+				date_id = -1
+
+				try:
+					date_id = date_list[date_key]
+				except KeyError:
+					date_list[date_key] = len(date_list)
+					date_id = date_list[date_key]
+					date_writer.writerow([date_id,Day_of_Week,Month,Day,Year])
+
+				#offense (will be used for calculating severity)
+				Offense = row[offense_index]
+				if(Offense == 'RAPE'):
+					Offense = 'CRIM SEXUAL ASSAULT'
+				elif(Offense == 'FELONY ASSAULT'):
+					Offense = 'ASSAULT'
+				elif('LARCENY' in Offense):
+					Offense = 'THEFT'
+				elif(Offense == 'MURDER'):
+					Offense = 'HOMICIDE'
+				offense_key = ','.join([Offense])
+				offense_id = -1
+				try:
+					offense_id = offense_list[offense_key]
+				except KeyError:
+					offense_list[offense_key] = len(offense_list)
+					offense_id = offense_list[offense_key]
+					offense_writer.writerow([offense_id,Offense])
+
+
+				#location
+				Location = eval(row[location_index])
+				Latitude = Location[0]
+				Longitude = Location[1]
+				location_key = ','.join([str(Location)])
+				location_id = -1
+				try:
+					location_id = location_list[location_key]
+				except KeyError:
+					location_list[location_key] = len(location_list)
+					location_id = location_list[location_key]
+					location_writer.writerow([location_id,Latitude,Longitude])
+
+				datime = row[date_index].split()
+				time = datime[1]+" "+ datime[2]
+				in_time = datetime.strptime(time, "%I:%M:%S %p")
+				out_time = datetime.strftime(in_time, "%H:%M")
+	 
+				fact_writer.writerow([ID,1,time_id,date_id,offense_id,location_id])
+				crime_writer.writerow([ID,Offense,Day_of_Week,datime[0],out_time])
+			
+
+	
+
+if __name__ == '__main__':
+	main()
