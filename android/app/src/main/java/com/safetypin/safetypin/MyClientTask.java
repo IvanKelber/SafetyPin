6412396a978e97dc88c4c85583601d3996f06a1e
Ronallen <ivan_kelber@brown.edu>
2016-05-11 19:16:23 -0400

    The crimes are now different colors!  You can also click on a pin to see what it's doing there.  i.e. what crime it is/ if it's an intersection in our path or not.

--- android/app/src/main/java/com/safetypin/safetypin/MyClientTask.java
+++ android/app/src/main/java/com/safetypin/safetypin/MyClientTask.java
@@ -16,6 +16,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 /**
  * Created by Ivan on 1/21/2016.
@@ -30,6 +32,8 @@ public class MyClientTask extends AsyncTask<Void, Void, Void> {
     private LatLng destination;
     OnEmptyResponseListener listener;
     private ArrayList<Marker> crimes;
+    final HashMap<Integer,String> CRIME_TYPES = new HashMap<>();
+    final HashMap<Integer,Float> CRIME_COLORS = new HashMap<>();
 
     public interface OnEmptyResponseListener {
         public void onEmptyReponse();
@@ -47,7 +51,33 @@ public class MyClientTask extends AsyncTask<Void, Void, Void> {
         this.mMap = mMap;
         this.source = source;
         this.destination = destination;
-        this.message = "("+convertToString(source) + "," + convertToString(destination)+")";
+        this.message = "(" + convertToString(source) + "," + convertToString(destination) + ")";
+
+        CRIME_TYPES.put(0,"Homicide");
+        CRIME_TYPES.put(1,"Sexual Assault");
+        CRIME_TYPES.put(2,"Robbery");
+        CRIME_TYPES.put(3,"Assault");
+        CRIME_TYPES.put(4,"Burglary");
+        CRIME_TYPES.put(5,"Theft");
+        CRIME_TYPES.put(6,"Battery");
+        CRIME_TYPES.put(7,"Weapons Violation");
+        CRIME_TYPES.put(8,"Offense Involving Children");
+        CRIME_TYPES.put(9,"Sexual Offense");
+        CRIME_TYPES.put(10,"Hate Crime");
+
+        CRIME_COLORS.put(0,BitmapDescriptorFactory.HUE_RED);
+        CRIME_COLORS.put(1,BitmapDescriptorFactory.HUE_ORANGE);
+        CRIME_COLORS.put(2,BitmapDescriptorFactory.HUE_GREEN);
+        CRIME_COLORS.put(3,BitmapDescriptorFactory.HUE_YELLOW);
+        CRIME_COLORS.put(4,BitmapDescriptorFactory.HUE_MAGENTA);
+        CRIME_COLORS.put(5,BitmapDescriptorFactory.HUE_ROSE);
+        CRIME_COLORS.put(6,BitmapDescriptorFactory.HUE_BLUE);
+        //The rest aren't used in NYC
+        CRIME_COLORS.put(7,BitmapDescriptorFactory.HUE_RED);
+        CRIME_COLORS.put(8,BitmapDescriptorFactory.HUE_RED);
+        CRIME_COLORS.put(9,BitmapDescriptorFactory.HUE_RED);
+        CRIME_COLORS.put(10,BitmapDescriptorFactory.HUE_RED);
+
     }
 
     private String convertToString(LatLng l) {
@@ -55,12 +85,13 @@ public class MyClientTask extends AsyncTask<Void, Void, Void> {
         double lng = l.longitude;
         return "(" + lat + "," + lng + ")";
     }
+
     @Override
     protected Void doInBackground(Void... args) {
         Socket socket = null;
         DataOutputStream dos = null;
         try {
-            socket = new Socket(dstAddress,dstPort);
+            socket = new Socket(dstAddress, dstPort);
             dos = new DataOutputStream(socket.getOutputStream());
             byte[] b = message.getBytes();
             dos.write(b);
@@ -71,7 +102,7 @@ public class MyClientTask extends AsyncTask<Void, Void, Void> {
             BufferedReader br = new BufferedReader(disR2);//create a BufferReader object for input
             StringBuilder sb = new StringBuilder();
             String line;
-            while((line = br.readLine()) != null) {
+            while ((line = br.readLine()) != null) {
                 sb.append(line);
                 sb.append("\n");
             }
@@ -86,15 +117,15 @@ public class MyClientTask extends AsyncTask<Void, Void, Void> {
 
     @Override
     protected void onPostExecute(Void result) {
-        if(response != null) {
+        if (response != null) {
 //            String[] fileArray = response.substring(1, response.length() - 2).split(",");
 //            textView.setText(response);
-            Log.v("RECEIVED RESPONSE:",response);
-            ArrayList<ArrayList<LatLng>> coords = parseResponse(response);
-            ArrayList<LatLng> waypoints = coords.get(0);
-            Log.e("WAYPOINTS:",waypoints.toString() + "" + waypoints.size());
+            Log.v("RECEIVED RESPONSE:", response);
+            ArrayList<LinkedHashMap<LatLng,Integer>> coords = parseResponse(response);
+            ArrayList<LatLng> waypoints = new ArrayList<>(coords.get(0).keySet());
+            Log.e("WAYPOINTS:", waypoints.toString() + "" + waypoints.size());
             if (waypoints.isEmpty()) {
-                Log.e("RECEIVED RESPONSE '[]'",response);
+                Log.e("RECEIVED RESPONSE '[]'", response);
                 listener.onEmptyReponse();
             }
             waypoints.add(0, source);
@@ -102,18 +133,18 @@ public class MyClientTask extends AsyncTask<Void, Void, Void> {
 
 //            LatLng l = new LatLng(Double.parseDouble(latlng[0]),Double.parseDouble(latlng[1]));
 //            Marker m = mMap.addMarker(new MarkerOptions().position(l).title("Destination").draggable(true));
-            ArrayList<ArrayList<LatLng>> segments = new ArrayList<ArrayList<LatLng>>();
+            ArrayList<ArrayList<LatLng>> segments = new ArrayList<>();
             segments.add(new ArrayList<LatLng>());
 
             for (int i = 0; i < waypoints.size(); i++) {
-                    if (i != 0 && i % 22 == 0) {
-                        segments.get(segments.size() - 1).add(waypoints.get(i));
-                        segments.add(new ArrayList<LatLng>());
-                    }
+                if (i != 0 && i % 22 == 0) {
+                    segments.get(segments.size() - 1).add(waypoints.get(i));
+                    segments.add(new ArrayList<LatLng>());
+                }
                 segments.get(segments.size() - 1).add(waypoints.get(i));
             }
             for (ArrayList wp : segments) {
-                DirectionsFetcher df = new DirectionsFetcher(mMap,(LatLng)wp.get(0),(LatLng)wp.get(wp.size()-1),wp);
+                DirectionsFetcher df = new DirectionsFetcher(mMap, (LatLng) wp.get(0), (LatLng) wp.get(wp.size() - 1), wp);
                 df.execute();
                 try {
                     Thread.sleep(2000);
@@ -123,45 +154,68 @@ public class MyClientTask extends AsyncTask<Void, Void, Void> {
             }
 //            fileListView.setFiles(Arrays.asList(fileArray));
             for (LatLng point : waypoints) {
-                mMap.addMarker(new MarkerOptions().position(point)
+                mMap.addMarker(new MarkerOptions().position(point).title("Intersection in Path")
                         .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
             }
 
-            for (LatLng point : coords.get(1)) {
-                crimes.add(mMap.addMarker(new MarkerOptions().position(point)
-                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)).visible(false)));
+            for (LatLng crime : coords.get(1).keySet()) {
+                Integer type = coords.get(1).get(crime);
+                crimes.add(mMap.addMarker(new MarkerOptions().position(crime).title(CRIME_TYPES.get(type))
+                        .icon(BitmapDescriptorFactory.defaultMarker(CRIME_COLORS.get(type))).visible(false)));
             }
         }
         super.onPostExecute(result);
     }
 
-    private ArrayList<ArrayList<LatLng>> parseResponse(String r) {
-        String k = r.substring(2,r.length()-2);
-        Log.e("SPLIT STRING:",k);
+    private ArrayList<LinkedHashMap<LatLng,Integer>> parseResponse(String r) {
+        String k = r.substring(2, r.length() - 2);
         String regex = "\\), ";
-        ArrayList<ArrayList<LatLng>> ret = new ArrayList<>();
-        ret.add(new ArrayList<LatLng>());
-        ret.add(new ArrayList<LatLng>());
+        ArrayList<LinkedHashMap<LatLng,Integer>> ret = new ArrayList<>();
+        ret.add(new LinkedHashMap<LatLng,Integer>());
+        ret.add(new LinkedHashMap<LatLng,Integer>());
         String[] coords = k.split("\\], \\[");
-        Log.e("WAYPOINTS:",coords[0]);
-        Log.e("CRIMES:",coords[1]);
+        Log.e("WAYPOINTS:", coords[0]);
+        Log.e("CRIMES:", coords[1]);
 
-        for (int i = 0; i < 2; i++) {
-            String[] splits = coords[i].split(regex);
-            for (String s : splits) {
-                try {
-                    if (!s.contains(")")) {
-                        s += ")";
-                    }
-                    Double lat = Double.parseDouble(s.substring(1, s.indexOf(",")));
-                    Double lng = Double.parseDouble(s.substring(s.indexOf(",") + 2, s.length() - 2));
-                    ret.get(i).add(new LatLng(lat, lng));
-                } catch (StringIndexOutOfBoundsException e) {
-                    Log.e("STRING INDEX OOB:", s);
+        String[] points = coords[0].split(regex);
+        for (String s : points) {
+
+            try {
+                if (!s.contains(")")) {
+                    s += ")";
                 }
+                Double lat = Double.parseDouble(s.substring(1, s.indexOf(",")));
+                Double lng = Double.parseDouble(s.substring(s.indexOf(",") + 2, s.length() - 2));
+                ret.get(0).put(new LatLng(lat, lng),0);
+            } catch (StringIndexOutOfBoundsException e) {
+                Log.e("STRING INDEX OOB:", s);
             }
-
         }
+
+
+        String[] crimeInfo = coords[1].split(regex);
+        for (String s : crimeInfo) {
+            try {
+                if (!s.contains(")")) {
+                    s += ")";
+                }
+                int type = Integer.parseInt(s.substring(1,s.indexOf(',')));
+                Double lat = Double.parseDouble(s.substring(ordinalIndexOf(s,',',0) + 2, ordinalIndexOf(s,',',1)));
+                Double lng = Double.parseDouble(s.substring(ordinalIndexOf(s,',',1) + 2, s.length() - 2));
+                ret.get(1).put(new LatLng(lat, lng),type);
+            } catch (StringIndexOutOfBoundsException e) {
+                Log.e("STRING INDEX OOB:", s);
+            }
+        }
+
         return ret;
+    }
+
+
+    public int ordinalIndexOf(String str, char c, int n) {
+        int pos = str.indexOf(c, 0);
+        while (n-- > 0 && pos != -1)
+            pos = str.indexOf(c, pos + 1);
+        return pos;
     }
 }
